fa1e1d7720c2db167038d33a04101b5e28ab93d2
Eladio <eladiojuniorrodriguez@gmail.com>
2020-03-08 20:46:37 -0400

    Initial Commit

---
+++ SearchItem/LinkedList.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EstructurasDeDatos.ListaEnlazada
+{
+    public class LinkedList
+    {
+        private Node head;
+        private Node tail;
+
+        public Node Head
+        {
+            get
+            {
+                return this.head;
+            }
+        }
+
+        public LinkedList()
+        {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void AddItemAtBeginning(int value)
+        {
+            Node node = new Node(value);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                node.Next = this.head;
+                this.head = node;
+            }
+        }
+
+        public void AddItemAtEnd(int value)
+        {
+            Node node = new Node(value);
+            if (this.head == null)
+            {
+                this.head = this.tail = node;
+            }
+            else
+            {
+                this.tail.Next = node;
+                this.tail = node;
+            }
+        }
+
+        public void RemoveItem(int item)
+        {
+            Node node = this.head;
+            if (node != null && node.Value == item)
+            {
+                this.head = this.head.Next;
+            }
+            else
+            {
+                node = this.SearchPreviousNode(item);
+                if (node != null)
+                {
+                    node.Next = node.Next?.Next;
+                }
+            }
+        }
+
+        public Node RemoveFirstItem()
+        {
+            Node node = this.head;
+            this.head = this.head?.Next;
+            return node;
+        }
+
+        public Node SearchItem(int item)
+        {
+            Node node = this.head;
+            while (node != null)
+            {
+                if (node.Value == item)
+                {
+                    return node;
+                }
+                node = node.Next;
+            }
+            return null;
+        }
+
+        public bool IsEmpty()
+        {
+            return this.head == null;
+        }
+
+        private Node SearchPreviousNode(int item)
+        {
+            Node node = this.head;
+            while (node != null)
+            {
+                if (node.Next?.Value == item)
+                {
+                    return node;
+                }
+                node = node.Next;
+            }
+            return null;
+        }
+    }
+}
---
+++ SearchItem/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using EstructurasDeDatos.ListaEnlazada;
+
+namespace SearchItem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+            Console.Write("Digite la cantidad de ITEMS: ");
+            int cantidad = int.Parse(Console.ReadLine());
+            int item, selected, counter = 0;
+
+            for (int i = 0; i < cantidad; i++)
+            {
+                Console.Write($"Item #{ i + 1}: ");
+                item = int.Parse(Console.ReadLine());
+                linkedList.AddItemAtBeginning(item);
+            }
+            Console.Write("Digite el elemento que desea contabilizar: ");
+            selected = int.Parse(Console.ReadLine());
+
+
+
+            Node node = linkedList.Head;
+
+            while (node != null)
+            {
+                Console.Write(node.Value + " -> ");
+                node = node.Next;
+            }
+            Console.WriteLine("");
+            LinkedList linkedList1 = new LinkedList();
+            linkedList1.AddItemAtBeginning(selected);
+            int x = 0;
+            node = linkedList.Head;
+
+            while (node != null)
+            {
+                if (node.Value == selected)
+                {
+                    x++;
+                }
+
+                node = node.Next;
+            }
+
+            Console.WriteLine($"Cantidad: {x}");
+        }
+    }
+}
